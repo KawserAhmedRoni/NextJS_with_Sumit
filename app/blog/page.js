23014f30d7da5c9f15423fb26fc12bac727e86b9
Kawser Ahmed Roni <kawserahmedroni51@gamil.com>
2024-08-20 01:19:08 +0600

    first commit

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,9 +1,11 @@
-export default function Home() {
+import React from "react";
+
+export default function Blog() {
 	return (
 		<main>
 			<div className="about-area min-h-screen py-10">
 				<div className="container mx-auto px-4">
-					<h1 className="text-4xl text-center">Home Page</h1>
+					<h1 className="text-4xl text-center">Blog Page</h1>
 				</div>
 			</div>
 		</main>
--- app/team/page.js
+++ app/team/page.js
@@ -1,9 +1,11 @@
-export default function Home() {
+import React from "react";
+
+export default function Team() {
 	return (
 		<main>
 			<div className="about-area min-h-screen py-10">
 				<div className="container mx-auto px-4">
-					<h1 className="text-4xl text-center">Home Page</h1>
+					<h1 className="text-4xl text-center">Team Page</h1>
 				</div>
 			</div>
 		</main>
